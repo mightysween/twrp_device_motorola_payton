6ba49668cad5725bf44a6be170d71df3b6cf8d9c
Rohan Taneja <rohantaneja63@gmail.com>
2017-12-21 00:55:30 +0530

    payton: initial setup for A/B
    bootctrl.sdm6xx needed for now a placeholder is set
    moved sbin to vendor/lib64
    based off to omni

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,5 @@
-
-
-# Copyright 2014 The Android Open Source Project
+#
+# Copyright 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,5 +14,12 @@
 # limitations under the License.
 #
 
-add_lunch_combo lineage_payton-userdebug
-add_lunch_combo lineage_payton-eng
+# This file is executed by build/envsetup.sh, and can use anything
+# defined in envsetup.sh.
+#
+# In particular, you can add lunch options with the add_lunch_combo
+# function: add_lunch_combo generic-eng
+
+for var in eng user userdebug; do
+  add_lunch_combo omni_payton-$var
+done
